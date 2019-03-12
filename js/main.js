71a05e49c20f25c91d3d21d1c19a9b4fbece2980
Reed Kavner <rik919@gmail.com>
2019-03-13 08:10:20 +1100

    Added GA click tracking

--- js/main.js
+++ js/main.js
@@ -1,46 +1,59 @@
-    //load the YT IFrame Player API code asynchronously
-    var tag = document.createElement('script');
-    tag.src = "https://www.youtube.com/iframe_api";
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+//load the YT IFrame Player API code asynchronously
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-    //create YT player
-    var player;
+//create YT player
+var player;
 
-    function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-            height: '390',
-            width: '640',
-            videoId: '26-nLZZGupU',
-            playerVars: { 'rel': 0, 'modestbranding': 1, 'playsinline' : 1},
-            events: { 'onStateChange': onPlayerStateChange }
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: '26-nLZZGupU',
+        playerVars: { 'rel': 0, 'modestbranding': 1, 'playsinline': 1 },
+        events: { 'onStateChange': onPlayerStateChange }
+    });
+}
+
+//open lightbox and play video
+$('#play').click(function() {
+    $('.lightbox').show();
+    player.playVideo();
+    $('body').addClass('overflow-hidden');
+});
+
+// close the lightbox if the user clicks on anything except the open button or yt player
+$(document).click(function(event) {
+    if (!$(event.target).closest(".embed-responsive, #play").length) {
+        player.stopVideo();
+        $('.lightbox').hide();
+    }
+});
+
+//close lightbox when video finishes
+function onPlayerStateChange(event) {
+    if (event.data === 0) {
+        $('body').removeClass('overflow-hidden');
+        $('.lightbox').hide();
+    }
+}
+
+$(document).ready(function() {
+
+    $('.ga-track').mousedown(function(event) {
+        var target = $(event.currentTarget);
+        var label = target.data('ga-label');
+        var ga = gtag('event', 'click', {
+            'event_label': label
         });
-    }
-
-    //open lightbox and play video
-    $('#play').click(function() {
-        $('.lightbox').show();
-        player.playVideo();
-        $('body').addClass('overflow-hidden');
-    });
-
-    // close the lightbox if the user clicks on anything except the open button or yt player
-    $(document).click(function(event) {
-        if (!$(event.target).closest(".embed-responsive, #play").length) {
-            player.stopVideo();
-            $('.lightbox').hide();
-        }
-    });
-
-    //close lightbox when video finishes
-    function onPlayerStateChange(event) {
-        if (event.data === 0) {
-            $('body').removeClass('overflow-hidden');
-            $('.lightbox').hide();
-        }
-    }
+        console.log('GA Click Event: ' + label);
+    })
 
     var quack = document.getElementById("quack");
     $(".footer-duck").click(function() {
         quack.play();
     })
+
+});
