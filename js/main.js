35797f5a6f701cb0b939606378a1a8b2990ad487
Reed Kavner <rik919@gmail.com>
2019-04-04 07:08:33 -0400

    Release day!

--- js/main.js
+++ js/main.js
@@ -1,46 +1,3 @@
-//load the YT IFrame Player API code asynchronously
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-//create YT player
-var player;
-
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        height: '390',
-        width: '640',
-        videoId: '26-nLZZGupU',
-        playerVars: { 'rel': 0, 'modestbranding': 1, 'playsinline': 1 },
-        events: { 'onStateChange': onPlayerStateChange }
-    });
-}
-
-//open lightbox and play video
-$('#play').click(function() {
-    $('.lightbox').show();
-    player.playVideo();
-    $('body').addClass('overflow-hidden');
-});
-
-// close the lightbox if the user clicks on anything except the open button or yt player
-$(document).click(function(event) {
-    if (!$(event.target).closest(".embed-responsive, #play").length) {
-        player.stopVideo();
-        $('body').removeClass('overflow-hidden');
-        $('.lightbox').hide();
-    }
-});
-
-//close lightbox when video finishes
-function onPlayerStateChange(event) {
-    if (event.data === 0) {
-        $('body').removeClass('overflow-hidden');
-        $('.lightbox').hide();
-    }
-}
-
 $(document).ready(function() {
 
     var playPromise = $('#bg-vid').get(0).play();
@@ -48,9 +5,14 @@ $(document).ready(function() {
     // playPromise won’t be defined.
     if (playPromise !== undefined) {
         playPromise.then(function() {
-            $('#bg-vid').show();
+            $('#bg-vid').fadeIn('fast');
         });
     }
+
+    // if video doesn't play in .5 seconds, show static bg
+    setTimeout(function(){
+        $('#hero').removeClass('loading') 
+    }, 500);
 
     $('.ga-track').mousedown(function(event) {
         var target = $(event.currentTarget);
